5faad8d65fd75a490badfc49d48cea16d4c5ee65
AlekseyYarina <aleksey.yarina@gmail.com>
2024-01-31 21:10:05 +0200

    feat: change state to homework

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
-import { persistor, store } from './redux-state/store';
+// import { persistor, store } from './redux-state/store';
+import { store } from './redux-state/store';
 import { App } from 'App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor} loading={null}>
-        <App />
-      </PersistGate>
+      {/* <PersistGate persistor={persistor} loading={null}> */}
+      <App />
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
--- src/redux-state/contacts/selectors.js
+++ src/redux-state/contacts/selectors.js
@@ -1,3 +1,3 @@
 export const selectFilter = store => store.contacts.filter;
-export const selectContacts = store => store.contacts.contacts;
-// export const selectContacts = store => store.contacts.contacts.items;
+// export const selectContacts = store => store.contacts.contacts;
+export const selectContacts = store => store.contacts.contacts.items;
